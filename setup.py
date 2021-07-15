58d10b316f65e5d5afdd1af9dbef353bbbf8dffb
Jeremie Coullon <jeremie.coullon@gmail.com>
2021-07-15 18:06:11 +0100

    bump patch version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ EXTRAS = {
 
 setup(
     name='SGMCMCJax',
-    version='0.1.0',
+    version='0.1.1',
     author='Jeremie Coullon',
     author_email='jeremie.coullon@gmail.com',
     packages=find_packages(".", exclude=["tests"]),
