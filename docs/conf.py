9d2f24ec3fbf72bc7427928e3bd1101c6351101b
Jeremie Coullon <jeremie.coullon@gmail.com>
2021-07-15 18:35:57 +0100

    bump doc version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Jeremie Coullon'
 author = 'Jeremie Coullon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
