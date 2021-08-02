cff83c7ce6de9196e33d3b3e2f94b525e5cc89be
Jeremie Coullon <jeremie.coullon@gmail.com>
2021-08-02 18:00:46 +0100

    0.2.0 release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ EXTRAS = {
 
 setup(
     name='SGMCMCJax',
-    version='0.1.4',
+    version='0.2.0',
     author='Jeremie Coullon',
     author_email='jeremie.coullon@gmail.com',
     packages=find_packages(".", exclude=["tests"]),
