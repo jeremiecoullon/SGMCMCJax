805f176da91681fff4dd2cbf8fbd994dada5853d
Jeremie Coullon <jeremie.coullon@gmail.com>
2021-08-11 11:28:28 +0100

    bumb to 0.2.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ EXTRAS = {
 
 setup(
     name='SGMCMCJax',
-    version='0.2.1',
+    version='0.2.2',
     author='Jeremie Coullon',
     author_email='jeremie.coullon@gmail.com',
     packages=find_packages(".", exclude=["tests"]),
