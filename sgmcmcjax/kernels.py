jeremiecoullon/SGMCMCJax

commit 1a43e5ea391139a52b634f68432e76f88cb2b1c5

Dev (#46)
* Setup CI (#44)
* new installation instructions and test coverage (#45)

sgmcmcjax/kernels.py
@@ -146,8 +146,9 @@ def build_sgld_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
+    init_diff, update_diff, get_p_diff = sgld(dt)
     init_fn, sgld_kernel, get_params = _build_langevin_kernel(
-        *sgld(dt), estimate_gradient, init_gradient
+        init_diff, update_diff, get_p_diff, estimate_gradient, init_gradient
     )
     return init_fn, sgld_kernel, get_params
 
@@ -177,8 +178,9 @@ def build_sgldCV_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn_CV(
         grad_log_post, data, batch_size, centering_value
     )
+    init_diff, update_diff, get_p_diff = sgld(dt)
     init_fn, sgldCV_kernel, get_params = _build_langevin_kernel(
-        *sgld(dt), estimate_gradient, init_gradient
+        init_diff, update_diff, get_p_diff, estimate_gradient, init_gradient
     )
     return init_fn, sgldCV_kernel, get_params
 
@@ -208,8 +210,9 @@ def build_sgld_SVRG_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn_SVRG(
         grad_log_post, data, batch_size, update_rate
     )
+    init_diff, update_diff, get_p_diff = sgld(dt)
     init_fn, sgldSVRG_kernel, get_params = _build_langevin_kernel(
-        *sgld(dt), estimate_gradient, init_gradient
+        init_diff, update_diff, get_p_diff, estimate_gradient, init_gradient
     )
     return init_fn, sgldSVRG_kernel, get_params
 
@@ -241,8 +244,9 @@ def build_psgld_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
+    init_diff, update_diff, get_p_diff = psgld(dt, alpha, eps)
     init_fn, sgld_kernel, get_params = _build_langevin_kernel(
-        *psgld(dt, alpha, eps), estimate_gradient, init_gradient
+        init_diff, update_diff, get_p_diff, estimate_gradient, init_gradient
     )
     return init_fn, sgld_kernel, get_params
 
@@ -276,8 +280,9 @@ def build_sgldAdam_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
+    init_diff, update_diff, get_p_diff = sgldAdam(dt, beta1, beta2, eps)
     init_fn, sgldAdam_kernel, get_params = _build_langevin_kernel(
-        *sgldAdam(dt, beta1, beta2, eps), estimate_gradient, init_gradient
+        init_diff, update_diff, get_p_diff, estimate_gradient, init_gradient
     )
     return init_fn, sgldAdam_kernel, get_params
 
@@ -307,8 +312,9 @@ def build_sgnht_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
+    init_diff, update_diff, get_p_diff = sgnht(dt, a)
     init_fn, sgnht_kernel, get_params = _build_langevin_kernel(
-        *sgnht(dt, a), estimate_gradient, init_gradient
+        init_diff, update_diff, get_p_diff, estimate_gradient, init_gradient
     )
     return init_fn, sgnht_kernel, get_params
 
@@ -340,8 +346,9 @@ def build_sgnhtCV_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn_CV(
         grad_log_post, data, batch_size, centering_value
     )
+    init_diff, update_diff, get_p_diff = sgnht(dt, a)
     init_fn, sgnht_kernel, get_params = _build_langevin_kernel(
-        *sgnht(dt, a), estimate_gradient, init_gradient
+        init_diff, update_diff, get_p_diff, estimate_gradient, init_gradient
     )
     return init_fn, sgnht_kernel, get_params
 
@@ -373,8 +380,13 @@ def build_baoab_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
+    init_diff, (update_diff1, update_diff1), get_p_diff = baoab(dt, gamma, tau)
     init_fn, baoab_kernel, get_params = _build_langevin_kernel(
-        *baoab(dt, gamma, tau), estimate_gradient, init_gradient
+        init_diff,
+        (update_diff1, update_diff1),
+        get_p_diff,
+        estimate_gradient,
+        init_gradient,
     )
     return init_fn, baoab_kernel, get_params
 
@@ -404,8 +416,13 @@ def build_badodab_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
+    init_diff, (update_diff1, update_diff1), get_p_diff = badodab(dt, a)
     init_fn, baoab_kernel, get_params = _build_langevin_kernel(
-        *badodab(dt, a), estimate_gradient, init_gradient
+        init_diff,
+        (update_diff1, update_diff1),
+        get_p_diff,
+        estimate_gradient,
+        init_gradient,
     )
     return init_fn, baoab_kernel, get_params
 
@@ -437,8 +454,13 @@ def build_badodabCV_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn_CV(
         grad_log_post, data, batch_size, centering_value
     )
+    init_diff, (update_diff1, update_diff1), get_p_diff = badodab(dt, a)
     init_fn, baoab_kernel, get_params = _build_langevin_kernel(
-        *badodab(dt, a), estimate_gradient, init_gradient
+        init_diff,
+        (update_diff1, update_diff1),
+        get_p_diff,
+        estimate_gradient,
+        init_gradient,
     )
     return init_fn, baoab_kernel, get_params
 
@@ -475,12 +497,16 @@ def build_sghmc_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
+    init_diff, update_diff, get_p_diff, resample_momentum = sghmc(dt, alpha)
     init_fn, sghmc_kernel, get_params = _build_sghmc_kernel(
-        *sghmc(dt, alpha),
+        init_diff,
+        update_diff,
+        get_p_diff,
+        resample_momentum,
         estimate_gradient,
         init_gradient,
         L,
-        compiled_leapfrog=compiled_leapfrog
+        compiled_leapfrog=compiled_leapfrog,
     )
     return init_fn, sghmc_kernel, get_params
 
@@ -516,12 +542,16 @@ def build_sghmcCV_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn_CV(
         grad_log_post, data, batch_size, centering_value
     )
+    init_diff, update_diff, get_p_diff, resample_momentum = sghmc(dt, alpha)
     init_fn, sghmc_kernel, get_params = _build_sghmc_kernel(
-        *sghmc(dt, alpha),
+        init_diff,
+        update_diff,
+        get_p_diff,
+        resample_momentum,
         estimate_gradient,
         init_gradient,
         L,
-        compiled_leapfrog=compiled_leapfrog
+        compiled_leapfrog=compiled_leapfrog,
     )
     return init_fn, sghmc_kernel, get_params
 
@@ -557,11 +587,15 @@ def build_sghmc_SVRG_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn_SVRG(
         grad_log_post, data, batch_size, update_rate
     )
+    init_diff, update_diff, get_p_diff, resample_momentum = sghmc(dt, alpha)
     init_fn, sghmc_kernel, get_params = _build_sghmc_kernel(
-        *sghmc(dt, alpha),
+        init_diff,
+        update_diff,
+        get_p_diff,
+        resample_momentum,
         estimate_gradient,
         init_gradient,
         L,
-        compiled_leapfrog=compiled_leapfrog
+        compiled_leapfrog=compiled_leapfrog,
     )
     return init_fn, sghmc_kernel, get_params

.github/workflows/makefile.yaml
@@ -1,0 +1,44 @@
+name: Makefile CI
+
+on: [push]
+jobs:
+  build:
+    name: build
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v2
+      
+    - name: Install dependencies
+      run: |
+        python setup.py develop --user
+        pip install -r requirements-dev.txt
+
+    - name: Run tests
+      run: |
+        pytest -v sgmcmcjax tests
+
+
+  lint:
+    name: linting
+    
+    runs-on: ubuntu-latest
+    
+    steps:
+    - uses: actions/checkout@v2
+
+    - name: Install dependencies
+      run: |
+        python setup.py develop --user
+        pip install -r requirements-dev.txt
+
+    - name: run mypy
+      run: mypy sgmcmcjax
+
+    - name: run black
+      run: black --diff --check $(git ls-files '*.py')
+
+    - name: run isort
+      run: isort .
+        

Makefile
@@ -1,5 +1,4 @@
-lint-checks:
-	mypy sgmcmcjax/
-
 test:
-		pytest -v sgmcmcjax tests
+	mypy sgmcmcjax/
+	pytest -v sgmcmcjax tests --cov=sgmcmcjax --cov-report=html
+

docs/conf.py
@@ -17,8 +17,8 @@ import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(0, os.path.abspath("../"))
-sys.path.append(os.path.abspath('..'))
-sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath(".."))
+sys.path.append(os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../../.."))
 
 # -- Project information -----------------------------------------------------

sgmcmcjax/diffusions.py
@@ -36,7 +36,7 @@
 #     A pytree representing the parameters extracted from `diffusion_state`, such that
 #     the invariant `params == get_params(init_fun(params))` holds true.
 # """
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Union
 
 import jax.numpy as jnp
 from jax import lax, random
@@ -45,7 +45,7 @@ from .diffusion_util import diffusion, diffusion_palindrome, diffusion_sghmc
 
 
 @diffusion
-def sgld(dt: float) -> Tuple[Callable, Callable, Callable]:
+def sgld(dt) -> Tuple[Callable, Callable, Callable]:
     """SGLD diffusion
     https://www.ics.uci.edu/~welling/publications/papers/stoclangevin_v6.pdf
 
@@ -75,7 +75,7 @@ def sgld(dt: float) -> Tuple[Callable, Callable, Callable]:
 
 @diffusion
 def psgld(
-    dt: float, alpha: float = 0.99, eps: float = 1e-5
+    dt, alpha: float = 0.99, eps: float = 1e-5
 ) -> Tuple[Callable, Callable, Callable]:
     """Preconditioned SGLD diffusion
     See algorithm 1 in paper: https://arxiv.org/pdf/1512.07666.pdf
@@ -114,7 +114,7 @@ def psgld(
 
 @diffusion
 def sgldAdam(
-    dt: float, beta1: float = 0.9, beta2: float = 0.999, eps: float = 1e-8
+    dt, beta1: float = 0.9, beta2: float = 0.999, eps: float = 1e-8
 ) -> Tuple[Callable, Callable, Callable]:
     """'ADAM'-like SGMCMC diffusion. See appendix in paper: https://arxiv.org/abs/2105.13059v1
 
@@ -159,7 +159,7 @@ def sgldAdam(
 
 @diffusion_sghmc
 def sghmc(
-    dt: float, alpha: float = 0.01, beta: float = 0
+    dt, alpha: float = 0.01, beta: float = 0
 ) -> Tuple[Callable, Callable, Callable, Callable]:
     """diffusion for stochastic gradient HMC.
     See paper: https://arxiv.org/abs/1402.4102. Uses the parametrisation in section G (appendix)
@@ -203,8 +203,8 @@ def sghmc(
 
 @diffusion_palindrome
 def baoab(
-    dt: float, gamma: float, tau: float = 1.0
-) -> Tuple[Callable, Callable, Callable, Callable]:
+    dt, gamma: float, tau: float = 1.0
+) -> Tuple[Callable, Tuple[Callable, Callable], Callable]:
     """BAOAB splitting scheme for the underdampled Langevin diffusion. https://aip.scitation.org/doi/abs/10.1063/1.4802990
 
     Args:
@@ -213,7 +213,7 @@ def baoab(
         tau (float, optional): temperature. Defaults to 1.
 
     Returns:
-        Tuple[Callable, Callable, Callable, Callable]: An (init_fun, (update1, update2), get_params) triple.
+        Tuple[Callable, Tuple[Callable, Callable], Callable]: An (init_fun, (update1, update2), get_params) triple.
     """
     dt = make_schedule(dt)
 
@@ -228,7 +228,7 @@ def baoab(
         x = x + v * dt(i) * 0.5
 
         c1 = jnp.exp(-gamma * dt(i))
-        c2 = jnp.sqrt(1 - c1 ** 2)
+        c2 = jnp.sqrt(1 - c1**2)
         v = c1 * v + tau * c2 * random.normal(k, shape=jnp.shape(v))
 
         x = x + v * dt(i) * 0.5
@@ -248,7 +248,7 @@ def baoab(
 
 
 @diffusion
-def sgnht(dt: float, a: float = 0.01) -> Tuple[Callable, Callable, Callable]:
+def sgnht(dt, a: float = 0.01) -> Tuple[Callable, Callable, Callable]:
     """Euler solver for the SG-NHT diffusion
     See algorithm 2 in http://people.ee.duke.edu/~lcarin/sgnht-4.pdf
 
@@ -295,8 +295,8 @@ def sgnht(dt: float, a: float = 0.01) -> Tuple[Callable, Callable, Callable]:
 
 @diffusion_palindrome
 def badodab(
-    dt: float, a: float = 0.01
-) -> Tuple[Callable, Callable, Callable, Callable]:
+    dt, a: float = 0.01
+) -> Tuple[Callable, Tuple[Callable, Callable], Callable]:
     """Splitting scheme for the 3-equation Langevin diffusion. See https://arxiv.org/abs/1505.06889
     This is a more stable discretisation than SG-NHT
 
@@ -305,7 +305,7 @@ def badodab(
         a (float, optional): initial value of alpha. Defaults to 0.01.
 
     Returns:
-        Tuple[Callable, Callable, Callable, Callable]: An (init_fun, update_fun, get_params) triple.
+        Tuple[Callable, Tuple[Callable, Callable], Callable]: An (init_fun, update_fun, get_params) triple.
     """
     dt = make_schedule(dt)
 
@@ -327,7 +327,7 @@ def badodab(
 
         c1 = jnp.exp(-alpha * dt(i))
         c2 = jnp.where(
-            alpha == 0, jnp.sqrt(dt(i)), jnp.sqrt(jnp.abs((1 - c1 ** 2) / (2 * alpha)))
+            alpha == 0, jnp.sqrt(dt(i)), jnp.sqrt(jnp.abs((1 - c1**2) / (2 * alpha)))
         )
         v = c1 * v + c2 * sigma * random.normal(k, shape=jnp.shape(v))
 
@@ -374,7 +374,7 @@ def cyclical_schedule(alpha_0: float, M: int, K: int) -> Callable:
     return schedule
 
 
-def make_schedule(scalar_or_schedule: Callable):
+def make_schedule(scalar_or_schedule: Union[float, Callable]) -> Callable:
     if callable(scalar_or_schedule):
         return scalar_or_schedule
     elif jnp.ndim(scalar_or_schedule) == 0:

sgmcmcjax/ksd.py
@@ -28,8 +28,8 @@ def k_0_fun(
     """
     diff = parm1 - parm2
     dim = parm1.shape[0]
-    base = c ** 2 + jnp.dot(diff, diff)
-    term1 = jnp.dot(gradlogp1, gradlogp2) * base ** beta
+    base = c**2 + jnp.dot(diff, diff)
+    term1 = jnp.dot(gradlogp1, gradlogp2) * base**beta
     term2 = -2 * beta * jnp.dot(gradlogp1, diff) * base ** (beta - 1)
     term3 = 2 * beta * jnp.dot(gradlogp2, diff) * base ** (beta - 1)
     term4 = -2 * dim * beta * (base ** (beta - 1))

sgmcmcjax/types.py
@@ -18,7 +18,7 @@ DiffusionState = namedtuple(
 register_pytree_node(
     DiffusionState,
     lambda xs: ((xs.packed_state,), (xs.tree_def, xs.subtree_defs)),
-    lambda data, xs: DiffusionState(xs[0], data[0], data[1]),
+    lambda data, xs: DiffusionState(xs[0], data[0], data[1]),  # type: ignore[index]
 )  # type: ignore[index]
 
 

commit b2b0b3034e8ae3f5944640470f5e86559bc90627

Fix BAOAB/BADODAB regression #69
Typo in commit 1a43e5ea391139a52b634f68432e76f88cb2b1c5 broke BAOAB and BADODAB. This fixes the typo.

sgmcmcjax/kernels.py
@@ -380,10 +380,10 @@ def build_baoab_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
-    init_diff, (update_diff1, update_diff1), get_p_diff = baoab(dt, gamma, tau)
+    init_diff, (update_diff1, update_diff2), get_p_diff = baoab(dt, gamma, tau)
     init_fn, baoab_kernel, get_params = _build_langevin_kernel(
         init_diff,
-        (update_diff1, update_diff1),
+        (update_diff1, update_diff2),
         get_p_diff,
         estimate_gradient,
         init_gradient,
@@ -416,10 +416,10 @@ def build_badodab_kernel(
     estimate_gradient, init_gradient = build_gradient_estimation_fn(
         grad_log_post, data, batch_size
     )
-    init_diff, (update_diff1, update_diff1), get_p_diff = badodab(dt, a)
+    init_diff, (update_diff1, update_diff2), get_p_diff = badodab(dt, a)
     init_fn, baoab_kernel, get_params = _build_langevin_kernel(
         init_diff,
-        (update_diff1, update_diff1),
+        (update_diff1, update_diff2),
         get_p_diff,
         estimate_gradient,
         init_gradient,